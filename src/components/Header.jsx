0584fbadfa068b13f28dcf1bcd4a266ae13be67a
grobert03 <gainarobert47@gmail.com>
2023-07-30 19:46:08 +0200

    Added tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="py-4 px-3">
+      <h2 className="font-bold text-4xl text-white">Country Explorer</h2>
+    </div>
+  )
+}
+
+export default Header;
